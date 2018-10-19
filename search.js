747c8ba57d6c008f36d9dc4a94532ef67ca1d783
sinyman <nyman.simon@hotmail.com>
2018-10-19 21:58:58 +0300

    Added some JS logic and changed up some HTML

---
+++ search.js
@@ -0,0 +1,20 @@
+function searchWords() {
+    markWords();
+}
+
+//Gets the words that match some query
+function getRegEx() {
+    // Getting regex query from HTML page
+    var query = document.getElementById("regexfield").value;
+    
+    // Returns an array containing the words found with the query
+    return new RegExp(query).exec;
+}
+
+// Marks all the words on the page that match query
+function markWords() {
+    // Gets the mathcing words for processing
+    var words = getRegEx();
+    
+    // More logic to come
+}
