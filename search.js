676c39523a28e715219f630d6eca7832fa629f06
Lakritspantern <22726715+Lakritspantern@users.noreply.github.com>
2018-10-27 14:43:25 +0300

    Slight refactor of JS code

--- search.js
+++ search.js
@@ -4,19 +4,21 @@ function searchWords() {
     markWords();
 }
 
-//Gets the words that match some query
-function getRegEx() {
-    // Getting regex query from HTML page
-    var query = document.getElementById("regexfield").value;
-    
-    // Returns an array containing the words found with the query
-    return new RegExp(query).exec;
+//Gets the query
+function getQuery() {
+    // Return regex query from HTML page
+    return document.getElementById("regexfield").value;
 }
 
 // Marks all the words on the page that match query
 function markWords() {
     // Gets the mathcing words for processing
-    var words = getRegEx();
+	var query = getQuery();
     
-    // More logic to come
+	// Get page source code
+	var html = document.body.innerHTML();
+
+	RegExp re = new RegExp(query);
+
+	// More code to come
 }
